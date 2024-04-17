384ade80576d76be17d887ab74ade5394ef8f645
Koniesjay <jkonyeri@gmail.com>
2024-04-17 23:28:08 +0100

    first commit

--- app/wallet/components/Header.jsx
+++ app/wallet/components/Header.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 const Header = () => {
   return (
-    <div className="mt-5">
+    <div className="mt-5 lg:my-5">
       <div className="flex place-content-center px-5">
         <Link href="/" className="flex justify-between items-center gap-x-2">
           <Image src="/token.svg" alt="pullix_logo" width={45} height={45} />
--- app/wallet/initialization/[id]/page.js
+++ app/wallet/initialization/[id]/page.js
@@ -9,9 +9,9 @@ import Image from "next/image";
 export default function page() {
   const { id } = useParams();
   const navigate = useRouter();
-  useEffect(() => {
-    setTimeout(() => navigate.push(`/wallet/importwallet/${id}`), 3000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => navigate.push(`/wallet/importwallet/${id}`), 3000);
+  // }, []);
   return (
     <div className="min-h-[100vh]">
       <div className="max-w-md mx-auto mt-10 w-[90%] bg-[#9022FF] px-6 pt-3 pb-10 rounded-3xl border-4 border-[#9022FF]">
@@ -30,19 +30,22 @@ export default function page() {
             }}
           />
         </div>
-        <div className="px-5 py-8 rounded-2xl border cursor-pointer border-[#FBD869] bg-[#FBD869] flex items-center justify-between">
-          <div className="flex flex-col gap-y-10 text-white">
-            <h3 className="font-bold text-2xl">{wallets[id].title}</h3>
-            <h5>Easy-to-Use browser extension</h5>
-          </div>
-          <div className="w-[30px] h-[30px]">
-            <Image
-              width={30}
-              height={30}
-              src={wallets[id].icon}
-              alt="logo.png"
-              className="w-full"
-            />
+        <div className="mt-11">
+          <div className="h-36 bg-[#ff9709] -mb-[10.9rem] ml-3 rounded-xl w-full"></div>
+          <div className="px-5 py-8 rounded-2xl border-[3px] cursor-pointer border-[#9022FF] bg-[#FBD869] flex items-center justify-between">
+            <div className="flex flex-col gap-y-10 text-white">
+              <h3 className="font-bold text-2xl">{wallets[id].title}</h3>
+              <h5>Easy-to-Use browser extension</h5>
+            </div>
+            <div className="w-[30px] h-[30px]">
+              <Image
+                width={30}
+                height={30}
+                src={wallets[id].icon}
+                alt="logo.png"
+                className="w-full"
+              />
+            </div>
           </div>
         </div>
       </div>
